cfe741c1d7b9b526050cdb1923a6bb2377765a65
Abraham <anubra266@gmail.com>
2022-01-12 17:02:45 +0100

    💄 update demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,16 +1,18 @@
-
-import { Stack } from "@chakra-ui/react";
+import { Stack, useColorMode, Button } from "@chakra-ui/react";
 import SimpleDate from "./simple-date";
 
 export default function App() {
+  const { toggleColorMode } = useColorMode();
   return (
     <Stack
+      py="10"
       w="300px"
       direction="column"
       pos="absolute"
       left="50%"
       transform="translateX(-50%)"
     >
+      <Button onClick={toggleColorMode}>Color Mode </Button>
       <SimpleDate />
     </Stack>
   );
